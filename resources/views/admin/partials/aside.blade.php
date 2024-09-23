e46a60104cb2d4c153cf18d7620fc68d4e2ba396
Francesco Dente <francesco.dente2003@gmail.com>
2024-09-23 16:10:47 +0200

    1.0.1: feat: db seeded, helper, routes and new app layout created

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,11 @@
+<aside class="text-bg-dark">
+
+    <ul>
+        <li>
+            <a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="{{ route('admin.posts.index') }}"><i class="fa-solid fa-list"></i> elenco post</a>
+            <a href="#"><i class="fa-solid fa-newspaper"></i> nuovo post</a>
+        </li>
+    </ul>
+
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,19 +19,23 @@ use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')
+    ->prefix('profile')
+    ->name('profile.')
+    ->group(function(){
+    Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
 });
 
-Route::middleware(['auth', 'verified'])
+Route::middleware(['auth','verified'])
     ->prefix('admin')
-    ->name('admin')
+    ->name('admin.')
     ->group(function(){
-        Route::get('/', [DashboardController::class, 'index'])->name('home');
+        Route::get('/',[DashboardController::class, 'index'])->name('home');
+
     });
+
+
 
 require __DIR__.'/auth.php';
